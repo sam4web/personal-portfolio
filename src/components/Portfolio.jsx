6cdfdee8843d7c35b32e625ea2d9e1f48ef9abcb
Sijal Manandhar <sijal.m06@gmail.com>
2023-11-02 17:44:51 +0545

    projects displayed in portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import { projectTags, projectList } from '../db/data.json';
 import { useState } from 'react';
+import { createReactIcon } from '../utils';
 
 const Portfolio = () => {
   const [projectTagList, setProjectTagList] = useState([]);
   const [filterTag, setFilterTag] = useState('all');
+  const [projcets, setProjects] = useState([]);
 
   useEffect(() => {
     const tagList = [{ id: 0, label: 'all', active: true }];
@@ -13,6 +15,16 @@ const Portfolio = () => {
     );
     setProjectTagList(tagList);
   }, []);
+
+  useEffect(() => {
+    setProjects(
+      projectList
+        .filter(
+          (project) => project.tags.includes(filterTag) || filterTag === 'all'
+        )
+        .slice(0, 9)
+    );
+  }, [projectTagList]);
 
   const changeFilterTag = (id) => {
     setProjectTagList((prevTagList) =>
@@ -33,31 +45,63 @@ const Portfolio = () => {
 
         <div className='content-container'>
           <ul className='project-tag__list'>
-            {projectTagList
-              ? projectTagList.map((tag) => (
-                  <li
-                    key={tag.id}
-                    className={`project-tag__item ${
-                      tag.active ? 'tag-active' : ''
-                    }`}
-                    onClick={() => {
-                      changeFilterTag(tag.id);
-                    }}
-                  >
-                    {tag.label}
-                  </li>
-                ))
-              : ''}
+            {projectTagList &&
+              projectTagList.map((tag) => (
+                <li
+                  key={tag.id}
+                  className={`project-tag__item ${
+                    tag.active ? 'tag-active' : ''
+                  }`}
+                  onClick={() => {
+                    changeFilterTag(tag.id);
+                  }}
+                >
+                  {tag.label}
+                </li>
+              ))}
           </ul>
           {/* /tags-container */}
 
           <div className='project-container'>
-            <div className='projcet-item'>
-              {projectList.map((project) => {
-                if (project.tags.includes(filterTag) || filterTag == 'all')
-                  return <div key={project.id}>{project.name}</div>;
-              })}
-            </div>
+            {projcets.map((project) => (
+              <div
+                className='project-item'
+                key={project.id}
+                title={project.name}
+              >
+                <div className='project-item__image'>
+                  <img src={project.imageLink} alt={project.name} />
+                </div>
+                <ul className='project-item__link-list'>
+                  {project.sourceCode && (
+                    <li className='project-item__link-item' title='source code'>
+                      <a
+                        href={project.sourceCode}
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        {createReactIcon('GithubIcon')}
+                      </a>
+                    </li>
+                  )}
+                  {project.livePreview && (
+                    <li
+                      className='project-item__link-item'
+                      title='live preview'
+                    >
+                      <a
+                        href={project.livePreview}
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        {createReactIcon('GlobeIcon')}
+                      </a>
+                    </li>
+                  )}
+                </ul>
+                <h4 className='project-item__title'> {project.name} </h4>
+              </div>
+            ))}
           </div>
           {/* /project-container */}
         </div>
